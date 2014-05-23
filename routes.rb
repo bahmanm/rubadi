de17d95e041bb3f280d50f38fe920c04fcafdf73
Bahman Movaqar <Bahman@BahmanM.com>
2014-05-23 09:50:13 +0430

    #11 Remove unused template Now building the string in 'routes.rb'

--- routes.rb
+++ routes.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
 require_relative 'models/rubadi'
-require 'erubis'
 
 get '/campaigns/:campaign_id' do
   campaign_id = params[:campaign_id].to_i
   model = Rubadi.new campaign_id, Time.now.min
   if model.valid_campaign then
     banner = url "/images/image_#{model.get_banner}.png"
-    erb :index, :locals => {:banner => banner}
+    "<img src='#{banner}'>"
   else
-    ""
+    ''
   end
 end
