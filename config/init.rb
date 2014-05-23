edd8c70cc3f21825731a35684ba2f41fd2cba722
Bahman Movaqar <Bahman@BahmanM.com>
2014-05-23 16:10:46 +0430

    #13 Enable logging Done. Just have to do `ruby main.rb 2>&1 > logfile`.

--- config/init.rb
+++ config/init.rb
@@ -1,6 +1,10 @@
 require 'sinatra/reloader' if development?
 
+before do
+  logger.datetime_format = "%Y/%m/%d %H:%M:%S "
+  logger.level = Logger::WARN
+end
+
 configure :development? do
   set :public_folder => File.dirname(__FILE__) + '/../static'
-  set :logging => true
 end
